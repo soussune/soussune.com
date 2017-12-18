dc4cc112141b6c8eafde43a85b604712362897ca
Masaya Kazama <miyan@t-p.jp>
2017-12-19 04:12:17 +0900

    Simplify mapping process

--- pages/actors/index.ts
+++ pages/actors/index.ts
@@ -1,37 +1,27 @@
 import { DateTime } from 'luxon'
 
 export default {
-  asyncData: async (context: any) => {
-    const app: any = context.app
+  asyncData: async (context) => {
+    const { app } = context
 
-    const appearMap: { [key: string]: any } = (await app
+    const appearMap = (await app
       .$content('/episode')
       .query({ exclude: [ 'meta', 'body', 'anchors', 'date' ] })
       .getAll())
-      .map((post) => {
-        post.date = DateTime.fromSQL(post.published).toJSDate()
-        return post
-      })
-      .reduce((map: any, post: any) => {
-        post.actorIds.forEach((actorId) => {
-          map[actorId] = [ ...(map[actorId] || []), post ]
+      .map((epsode) => ({ ...epsode, date: DateTime.fromSQL(epsode.published).toJSDate() }))
+      .reduce((map, epsode) => {
+        epsode.actorIds.forEach((actorId) => {
+          map[actorId] = [ ...(map[actorId] || []), epsode ]
         })
         return map
       }, {})
 
-    const actors: any = (await app
-      .$content('/actors')
-      .query({ exclude: [ 'meta', 'body', 'anchors', 'date' ] })
-      .getAll())
-      .map((actor) => {
-        actor.appears = appearMap[actor.actorId] || []
-        return actor
-      })
+    const actors = (await app.$content('/actors').query({ exclude: [ 'meta', 'body', 'anchors', 'date' ] }).getAll())
+      .map((actor) => ({ ...actor, appears: appearMap[actor.actorId] || [] }))
       .sort((a, b) => {
         // sort by appearCount or lastAppearDate
         const countDiff = b.appears.length - a.appears.length
-        if (countDiff !== 0) return countDiff
-        return b.appears[0].date - a.appears[0].date
+        return countDiff !== 0 ? countDiff : b.appears[0].date - a.appears[0].date
       })
 
     return { actors }

c9acc28cb5a0a584de6197217df2007c968afc98
Masaya Kazama <miyan@t-p.jp>
2017-12-21 01:21:36 +0900

    Show actors in episode

--- pages/episode/_episode.vue
+++ pages/episode/_episode.vue
@@ -1,17 +1,58 @@
 <template>
   <section class="content-container" ref="content">
-    <h1 class="post-title"> {{ post.title }} </h1>
-    <nuxtent-body :body="post.body" />
+    <h1 class="post-title"> {{ episode.title }} </h1>
+
+    <h2>内容紹介</h2>
+    {{ episode | desc }}
+
+    <h2>出演者</h2>
+    <ul class="actor-list">
+      <li
+        v-for="actor in episode.actors"
+        :key="actor.title"
+        class="actor-list-item"
+      >
+        <nuxt-link :to="actor.permalink">
+          <img
+            :src="actor.imageUrl"
+            :title="actor.title"
+          >
+          <p>{{ actor.title }}</p>
+        </nuxt-link>
+      </li>
+    </ul>
+
+    <nuxtent-body :body="episode.body" class="marked" />
   </section>
 </template>
 
 <script lang="ts" src="./_episode.ts">
 </script>
 
-<style>
+<style lang="scss" scoped>
+.actor-list {
+
+  &-item {
+    display: inline-block;
+    margin-bottom: 1rem;
+    text-align: center;
+    min-width: 100px;
+    font-size: 14px;
+
+    & a {
+      text-decoration: none;
+    }
+
+    & img {
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+    }
+  }
+}
 .content-container
 {
-  width: 38rem;
+  max-width: 38rem;
   margin: 0 auto
 }
 

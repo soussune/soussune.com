bf7d49f0c7f2321ccd6f81b0471f7ce58d328031
Masaya Kazama <miyan@t-p.jp>
2017-12-24 12:14:05 +0900

    Update ep header

--- pages/episode/_id.vue
+++ pages/episode/_id.vue
@@ -1,80 +1,73 @@
 <template>
-  <section
+  <main
     class="page-container"
     ref="content"
   >
-    <nav class="ep-links">
-      <div>
+    <section class="summary">
+
+      <header class="header">
+        <h1 class="title"> {{ episode.title }} </h1>
+        <p class="date">{{episode | date }}</p>
+      </header>
+
+      <div class="play">
+        <button @click="togglePlay" :class="{played: played}">
+          <icon scale="3" :name="played ? (canplay ? 'pause' : 'spinner') : 'play'" :spin="played && !canplay"></icon>
+        </button>
+      </div>
+
+      <div class="desc">
+        <h2>内容紹介</h2>
+        {{ episode | desc }}
+      </div>
+
+      <div class="actors">
+        <h2>出演者</h2>
+        <ul class="actor-list">
+          <li
+            v-for="actor in episode.actors"
+            :key="actor.title"
+            class="actor-list-item"
+          >
+            <nuxt-link :to="actor.permalink">
+              <img
+                :src="actor.imageUrl"
+                :title="actor.title"
+              >
+              <p>{{ actor.title }}</p>
+            </nuxt-link>
+          </li>
+        </ul>
+      </div>
+
+      <nav class="ep-links">
         <nuxt-link
           v-if="episode.older"
           :to="episode.older.permalink"
+          class="older"
         >
-          <icon name="chevron-circle-left" scale="2"></icon>
+          <icon name="arrow-left" scale="3"></icon>
         </nuxt-link>
-      </div>
-      <div>
         <nuxt-link
           v-if="episode.newer"
           :to="episode.newer.permalink"
+          class="newer"
         >
-          <icon name="chevron-circle-right" scale="2"></icon>
+          <icon name="arrow-right" scale="3"></icon>
         </nuxt-link>
-      </div>
-    </nav>
+      </nav>
 
-    <header class="header">
-      <h1 class="title"> {{ episode.title }} </h1>
-      <p class="date">{{episode | date }}</p>
-    </header>
-
-    <section class="summary">
-
-    <div class="play">
-      <button @click="togglePlay" :class="{played: played}">
-        <span v-if="played">
-          <icon scale="2" :name="canplay ? 'pause' : 'spinner'" :spin="!canplay"></icon>
-        </span>
-        <span v-else><icon name="play" scale="2"></icon></span>
-      </button>
-    </div>
-
-    <div class="desc">
-      <h2>内容紹介</h2>
-      {{ episode | desc }}
-    </div>
-
-    <div class="actors">
-      <h2>出演者</h2>
-      <ul class="actor-list">
-        <li
-          v-for="actor in episode.actors"
-          :key="actor.title"
-          class="actor-list-item"
-        >
-          <nuxt-link :to="actor.permalink">
-            <img
-              :src="actor.imageUrl"
-              :title="actor.title"
-            >
-            <p>{{ actor.title }}</p>
-          </nuxt-link>
-        </li>
-      </ul>
-    </div>
     </section>
 
     <nuxtent-body :body="episode.body" class="marked" />
-  </section>
+
+  </main>
 </template>
 
 <script lang="ts" src="./_id.js">
 </script>
 
 <style lang="scss" scoped>
-.ep-links {
-  display: flex;
-  justify-content: space-between;
-}
 .header {
   text-align: center;
 
@@ -88,19 +81,40 @@
 
 .summary {
   display: grid;
-  grid-template-areas: "play desc" "play actors";
+  grid-template-areas: "header header" "play desc" "play actors";
   grid-template-columns: 200px auto;
   margin-top: 40px;
 
   background: #fff;
   border-radius: .6rem;
   box-shadow: 0 1.2rem 3.6rem rgba(0,0,0,.2);
-  padding: 10px;
+  padding: 10px 20px;
+  position: relative;
+
+  .ep-links {
+    $offset: -20px;
+    & a {
+      position: absolute;
+      top: 160px;
+      color: rgba(0,0,0,.4);
+
+      &.older {
+        left: $offset;
+      }
+      &.newer {
+        right: $offset;
+        text-align: right;
+      }
+    }
+  }
 
   & h2 {
     font-size: 24px;
   }
 
+  .header {
+    grid-area: header;
+  }
   .play {
     grid-area: play;
     position: relative;
@@ -117,12 +131,13 @@
       color: #fff;
       font-size:18px;
       border: none;
-      border-radius: 20px;
+      border-radius: 50%;
       width: 100px;
-      height: 70px;
+      height: 100px;
       outline: none;
 
       &.played {
+        border-radius: 20%;
         background: #c35922
       }
     }
@@ -138,7 +153,7 @@
 
 .actor-list {
 
-&-item {
+  &-item {
     display: inline-block;
     margin-bottom: 1rem;
     text-align: center;

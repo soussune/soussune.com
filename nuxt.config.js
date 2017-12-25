e614b9b6a7b2c4323c1e73278b85db04ebbc2589
trkw <naohiro.terakawa@me.com>
2017-12-25 17:09:33 +0900

    Add default favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,10 +60,31 @@ module.exports = {
       { property: 'og:description', content: 'テクノロジーと世の中についてエンジニア達が雑談するポッドキャストです。', hid: 'ogDesc' },
 
       // Apple iTunes
-      { name: 'apple-itunes-app', content: 'app-id=1247135079' }
+      { name: 'apple-itunes-app', content: 'app-id=1247135079' },
+
+      // Microsoft
+      { name: 'msapplication-TileColor', content: '#1c2260' },
+      { name: 'msapplication-TileImage', content: '/mstile-144x144.png' }
+
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '57x57', href: '/apple-touch-icon-57x57.png' },
+      { rel: 'apple-touch-icon', sizes: '60x60', href: '/apple-touch-icon-60x60.png' },
+      { rel: 'apple-touch-icon', sizes: '72x72', href: '/apple-touch-icon-72x72.png' },
+      { rel: 'apple-touch-icon', sizes: '76x76', href: '/apple-touch-icon-76x76.png' },
+      { rel: 'apple-touch-icon', sizes: '114x114', href: '/apple-touch-icon-114x114.png' },
+      { rel: 'apple-touch-icon', sizes: '120x120', href: '/apple-touch-icon-120x120.png' },
+      { rel: 'apple-touch-icon', sizes: '144x144', href: '/apple-touch-icon-144x144.png' },
+      { rel: 'apple-touch-icon', sizes: '152x152', href: '/apple-touch-icon-152x152.png' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon-180x180.png' },
+      { rel: 'mask-icon', color: '#1c2260', href: '/website_icon.svg' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '48x48', href: '/favicon-48x48.png' },
+      { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
+      { rel: 'icon', type: 'image/png', sizes: '160x160', href: '/favicon-160x160.png' },
+      { rel: 'icon', type: 'image/png', sizes: '196x196', href: '/favicon-196x196.png' },
       { rel: 'alternate', type: 'application/rss+xml', href: '/feed.xml' }
     ]
   },

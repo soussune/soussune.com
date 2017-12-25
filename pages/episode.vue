064f09530e7baf83d63cb46ad129d2a0517c32e1
Masaya Kazama <miyan@t-p.jp>
2017-12-25 11:27:44 +0900

    Add slide transition between episode

--- pages/episode.vue
+++ pages/episode.vue
@@ -3,6 +3,13 @@
 </template>
 
 <script>
+export default {
+  transition (to, from) {
+    return (!from || from.name !== to.name)
+    ? 'page'
+    : Number(to.params.id) < Number(from.params.id) ? 'slide-right' : 'slide-left'
+  }
+}
 </script>
 
 <style>

7eee7db52955df872c3140537ced73020ac394d0
Masaya Kazama <miyan@t-p.jp>
2017-12-25 12:49:15 +0900

    Update episode list page template

--- pages/episode/index.ts
+++ pages/episode/index.ts
@@ -35,7 +35,7 @@ export default {
   },
   head () {
     return {
-      title: 'All episodes'
+      title: 'Episode list'
     }
   }
 }
--- pages/episode/index.vue
+++ pages/episode/index.vue
@@ -1,52 +1,69 @@
 <template>
   <section class="page-container">
-    <div>
-      <h2 class="home-slogan">
-        all episodes
-      </h2>
-      <div v-if="$store.state.queries.length > 0">
-        <icon name="search" scale="1.5"></icon> 検索中:（{{`${episodes.length}件`}}）
-        <button
-          v-for="(query, i) in $store.state.queries"
-          :key="query"
-          class="query"
-          @click="removeQuery(i)"
-        >
-          {{query}} <icon name="times" scale="1"></icon>
-        </button>
-      </div>
+    <header>
+      <h1 class="home-slogan">
+        Episode list
+      </h1>
+    </header>
+
+    <div v-if="$store.state.queries.length > 0">
+      <icon name="search" scale="1.5"></icon> 検索中:（{{`${episodes.length}件`}}）
+      <button
+        v-for="(query, i) in $store.state.queries"
+        :key="query"
+        class="query"
+        @click="removeQuery(i)"
+      >
+        {{query}} <icon name="times" scale="1"></icon>
+      </button>
+    </div>
+
+    <main>
       <div v-if="episodes.length > 0">
-        <div v-for="episode in episodes" :key="episode.title">
-          <h3 :id="'ep' + episode.id">
-            <nuxt-link :to="episode.permalink">
+        <nuxt-link
+          :to="episode.permalink"
+          v-for="episode in episodes"
+          :key="episode.permalink"
+          :id="'ep' + episode.id"
+          class="episode"
+        >
+          <article>
+            <h2 class="episode-header">
               {{ episode.title }}
-            </nuxt-link>
-          </h3>
-          <div>
-            {{ episode | date }}
-          </div>
-          <div>
-            {{ episode | desc }}
-          </div>
-          <div class="actor-list">
-            <nuxt-link
-              v-for="actor in episode.actors"
-              :key="actor.actorId"
-              :to="actor.permalink"
-              class="actor-list-item"
-            >
+            </h2>
+
+            <div class="episode-date">
+              {{ episode | date }}
+            </div>
+
+            <div class="episode-actor">
               <img
+                class="episode-actor-item"
+                v-for="actor in episode.actors"
+                :key="actor.actorId"
                 :src="actor.imageUrl"
                 :title="actor.title"
               >
-            </nuxt-link>
-          </div>
-        </div>
+            </div>
+
+            <div class="episode-topic">
+              <span
+                class="episode-topic-item"
+                v-for="topic in episode.topics"
+                :key="topic"
+              >
+                {{ topic }}
+              </span>
+            </div>
+
+          </article>
+        </nuxt-link>
       </div>
       <div v-else>
         No articles found.
       </div>
-    </div>
+    </main>
+
   </section>
 </template>
 
@@ -54,6 +71,54 @@
 </script>
 
 <style lang="scss" scoped>
+.episode {
+  text-decoration: none;
+
+  article {
+    display: grid;
+    grid-template-areas: "header actor" "topic date";
+    grid-template-columns: 1fr 160px;
+    border-bottom: 1px solid;
+  }
+  &-header {
+    grid-area: header;
+    font-size: 1.2rem;
+  }
+  &-actor {
+    grid-area: actor;
+    justify-self: end;
+
+    &-item {
+      margin-left: -12px;
+      border-radius: 50%;
+      border: 2px solid #fff;
+      box-sizing: content-box;
+      background-origin: content-box;
+      width: 30px;
+      height: 30px;
+    }
+
+  }
+  &-date {
+    grid-area: date;
+    font-size: 0.7rem;
+    justify-self: end;
+  }
+  &-topic {
+    grid-area: topic;
+    font-size: 0.8rem;
+
+    &-item {
+      align-items: center;
+      padding: 0 0.5rem;
+      background-color: #dfe3e8;
+      border-radius: 1rem;
+      color: #454f5b;
+    }
+  }
+
+}
+
 .query {
   padding: 0 10px;
   font-size: 16px;
@@ -62,23 +127,7 @@
   line-height: 40px;
   outline: none;
 }
-.actor-list {
-  margin-top: 8px;
-  margin-left: 10px;
 
-  &-item {
-    margin-left: -12px;
-
-    & img {
-      border-radius: 50%;
-      border: 2px solid #fff;
-      box-sizing: content-box;
-      background-origin: content-box;
-      width: 40px;
-      height: 40px;
-    }
-  }
-}
 .home-container
 {
   display: flex;

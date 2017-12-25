81f389edb8fa462ac7155a8709c21443b10d5d87
Masaya Kazama <miyan@t-p.jp>
2017-12-26 02:32:55 +0900

    Add episode body text to search target

--- store/index.js
+++ store/index.js
@@ -18,15 +18,17 @@ export const getters = {
     return { ...state.episodes[i], newer: state.episodes[i - 1], older: state.episodes[i + 1] }
   },
   filteredEpisodes: (state) => {
-    if (state.queries.length === 0) return state.episodes
-
-    const ret = state.episodes.filter((ep) =>
-      state.queries.every((q) => {
-        const r = new RegExp(q, 'i')
-        return ep.actorIds.some((a) => a.match(r)) || ep.title.match(r) || ep.topics.some((t) => t.match(r))
-      })
-    )
-    return ret
+    return state.queries.length === 0
+      ? state.episodes
+      : state.episodes.filter((ep) =>
+          state.queries.every((q) => {
+            const r = new RegExp(q, 'i')
+            return (
+              ep.actorIds.some((a) => a.match(r)) ||
+              ep.topics.some((t) => t.match(r) || ep.title.match(r) || ep.bodyText.match(r))
+            )
+          })
+        )
   }
 }
 export const mutations = {
@@ -49,7 +51,8 @@ export const actions = {
       .getAll()).map((e) => ({
         ...e,
         published: DateTime.fromSQL(e.published).valueOf(),
-        id: e.path.replace(/^.*\//, '')
+        id: e.path.replace(/^.*\//, ''),
+        bodyText: e.body.replace(/<[^>]*?>/g, ' ')
       }))
 
     const appearMap = episodes.reduce((map, epsode) => {

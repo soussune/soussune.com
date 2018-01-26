b1f5b546990ac448e938857ac155b71977f57df2
Masaya Kazama <miyan@t-p.jp>
2018-01-26 19:18:35 +0900

    Create tweet, form link component

---
+++ components/FeedbackForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <QueryLink
+    path='https://docs.google.com/forms/d/e/1FAIpQLSe4p8EuJIocQ_PJl7qe-lZ--pD7AN3LCk4FefDW0DmSBTBHIA/viewform'
+    :params='params'
+  ><slot/></QueryLink>
+</template>
+
+<script>
+import QueryLink from '~/components/QueryLink.vue'
+
+export default {
+  components: {
+    QueryLink
+  },
+  props: {
+    entryTitle: { type: String, default: undefined }
+  },
+  computed: {
+    params() {
+      return {
+        usp: 'sf_link',
+        'entry.286431956': this.entryTitle
+      }
+    }
+  }
+}
+</script>
---
+++ components/FeedbackTweet.vue
@@ -0,0 +1,30 @@
+<template>
+  <QueryLink
+    path='https://twitter.com/intent/tweet'
+    :params='params'
+  ><slot/></QueryLink>
+</template>
+
+<script>
+import QueryLink from '~/components/QueryLink.vue'
+
+export default {
+  components: {
+    QueryLink
+  },
+  props: {
+    hashtags: { type: String, default: 'soussune' },
+    url: { type: String, default: 'https://soussune.com' },
+    text: { type: String, default: undefined }
+  },
+  computed: {
+    params() {
+      return {
+        hashtags: this.hashtags,
+        url: this.url,
+        text: this.text
+      }
+    }
+  }
+}
+</script>

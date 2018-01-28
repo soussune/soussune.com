8c0a661519eb9269c874c328699daf0139644516
Masaya Kazama <miyan@t-p.jp>
2018-01-28 17:28:34 +0900

    Update style

--- pages/actors/_id.vue
+++ pages/actors/_id.vue
@@ -2,10 +2,10 @@
   <main
     class="page-container"
   >
-    <header class="title">
+    <header class="actor">
       <ActorIcon
         v-model="actor"
-        class="icon"
+        class="actor-icon"
       />
       <h1 class="actor-title"> {{ actor.title }} </h1>
     </header>
@@ -17,7 +17,7 @@
         class="button"
         target="_blank"
       >
-        <icon name="twitter" scale="1.1"/>
+        <icon name="twitter" class="twitter" scale="1.6"/>
         Twitter
       </a>
       <a
@@ -26,35 +26,37 @@
         class="button"
         target="_blank"
       >
-        <icon name="github" scale="1.1"/>
+        <icon name="github" class="github" scale="1.6"/>
         GitHub
       </a>
     </section>
 
-    <section class="desc">
-      <p>{{actor.description}}</p>
+    <section class="desc" v-if="actor.description">
+      {{actor.description}}
     </section>
 
-    <section class="body">
+    <section class="body" v-if="actor.body">
       <nuxtent-body :body="actor.body" class="marked" />
     </section>
 
     <section class="episodes">
-      <h2>episodes</h2>
-      <ul v-if="0 < actor.episodes.length">
-        <li
-          v-for="ep in actor.episodes"
-          :key="ep.permalink"
-          class="actor-list-item"
-        >
-          <nuxt-link :to="ep.permalink">
-            {{ ep.title }}
-          </nuxt-link>
-        </li>
-      </ul>
-      <p v-else>
-        not appeared yet.
-      </p>
+      <h2 class="episodes-title">出演エピソード</h2>
+      <div class="episodes-list">
+        <ul v-if="0 < actor.episodes.length">
+          <li
+            v-for="ep in actor.episodes"
+            :key="ep.permalink"
+            class="actor-list-item"
+          >
+            <nuxt-link :to="ep.permalink">
+              {{ ep.title }}
+            </nuxt-link>
+          </li>
+        </ul>
+        <p v-else>
+          （未登場）
+        </p>
+      </div>
     </section>
   </main>
 </template>
@@ -88,31 +90,32 @@ export default {
   border-radius: 0.6rem;
   box-shadow: 0 1.2rem 3.6rem rgba(0, 0, 0, 0.2);
   padding: 80px 60px;
-  margin: 0 20px;
   position: relative;
   text-align: left;
+  font-size: 1.1rem;
 
   display: grid;
-  grid-gap: 1rem;
+  grid-gap: 2rem;
 
   @include mq() {
     padding: 20px 10px 40px;
     margin: 20px 0px;
   }
 }
-.title {
+.actor {
   text-align: center;
-}
-.icon {
-  width: 200px;
-  height: 200px;
+
+  &-icon {
+    width: 200px;
+    height: 200px;
+  }
+  &-title {
+    font-size: 2.5rem;
+    margin-bottom: 1rem;
+    color: $clr-black-dd;
+  }
 }
 
-.actor-title {
-  font-size: 2.5rem;
-  margin-bottom: 1rem;
-  color: $clr-black-dd;
-}
 .accounts {
   display: grid;
   grid-gap: 1em;
@@ -124,6 +127,17 @@ export default {
   .button {
     @import '~assets/css/_roundButton.scss';
     border: 2px solid $clr-black;
+  }
+}
+.episodes {
+  &-title {
+    color: $clr-black-d;
+    text-align: center;
+    font-size: 1.8rem;
+  }
+  &-list {
+    width: max-content;
+    margin: 0 auto;
   }
 }
 </style>
--- pages/actors/index.vue
+++ pages/actors/index.vue
@@ -20,8 +20,8 @@
             </h2>
             <p class="actor-description">出演回数: {{ actor.episodes.length }}</p>
             <div class="actor-footer">
-              <icon name="github" class="github" scale="1.6" v-if="actor.githubUsername"></icon>
               <icon name="twitter" class="twitter" scale="1.6" v-if="actor.twitterUsername"></icon>
+              <icon name="github" class="github" scale="1.6" v-if="actor.githubUsername"></icon>
               <button class="actor-button">もっと詳しく</button>
             </div>
           </div>

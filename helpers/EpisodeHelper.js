05d8f9e12e297fb553517aa2d1956858e340dad3
Masaya Kazama <miyan@t-p.jp>
2018-01-31 15:42:27 +0900

    Support no-topic episode description

--- helpers/EpisodeHelper.js
+++ helpers/EpisodeHelper.js
@@ -4,10 +4,12 @@ const desc = (episode) => {
   const combinedActors = episode.actorIds.join('と')
   const postActors = episode.actorIds.length > 1 ? `の${episode.actorIds.length}人で` : 'が'
 
-  const combinedTopics = episode.topics.join('、')
-  const postTopics = episode.topics.length > 1 ? 'など' : ''
+  const topics = episode.topics || []
+  const combinedTopics = topics.join('、')
+  const postTopics = topics.length > 1 ? 'など' : ''
+  const topicsText = topics.length > 0 ? `、${combinedTopics}${postTopics}について` : ''
 
-  return `${combinedActors}${postActors}、${combinedTopics}${postTopics}について話しました。`
+  return `${combinedActors}${postActors}${topicsText}話しました。`
 }
 
 module.exports = { desc }

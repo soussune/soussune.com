362dc73f55a5b1b61f82a2296baff79954f4d731
trkw <naohiro.terakawa@me.com>
2018-02-02 08:51:40 +0900

    Add onesignal

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,6 +19,7 @@ const conf = {
   },
   modules: [
     '@nuxtjs/pwa',
+    '@nuxtjs/onesignal',
     '@nuxtjs/sentry',
     '~/modules/rss',
     '~/modules/typescript',
@@ -199,6 +200,43 @@ const conf = {
   },
   rssItems: episodes.episode,
   loading: { color: '#3B8070' },
+  oneSignal: {
+    init: {
+      appId: '0c93cec2-86ac-419b-a0f1-a1b41ee6172e',
+      allowLocalhostAsSecureOrigin: true,
+      autoRegister: false,
+      welcomeNotification: {
+        disable: true
+      },
+      notifiyButton: {
+        enable: true,
+        size: 'medium' /* One of 'small', 'medium', or 'large' */,
+        theme: 'default' /* One of 'default' (red-white) or 'inverse" (white-red) */,
+        position: 'bottom-left' /* Either 'bottom-left' or 'bottom-right' */,
+        offset: {
+          bottom: '0px',
+          left: '0px' /* Only applied if bottom-left */,
+          right: '0px' /* Only applied if bottom-right */
+        },
+        prenotify: true /* Show an icon with 1 unread message for first-time site visitors */,
+        showCredit: false /* Hide the OneSignal logo */,
+        text: {
+          'tip.state.unsubscribed': 'Subscribe to notifications',
+          'tip.state.subscribed': "You're subscribed to notifications",
+          'tip.state.blocked': "You've blocked notifications",
+          'message.prenotify': 'Click to subscribe to notifications',
+          'message.action.subscribed': 'Thanks for subscribing!',
+          'message.action.resubscribed': "You're subscribed to notifications",
+          'message.action.unsubscribed': "You won't receive notifications again",
+          'dialog.main.title': 'Manage Site Notifications',
+          'dialog.main.button.subscribe': 'SUBSCRIBE',
+          'dialog.main.button.unsubscribe': 'UNSUBSCRIBE',
+          'dialog.blocked.title': 'Unblock Notifications',
+          'dialog.blocked.message': 'Follow these instructions to allow notifications:'
+        }
+      }
+    }
+  },
   sentry: {
     config: {
       shouldSendCallback: () => process.env.NODE_ENV === 'production'

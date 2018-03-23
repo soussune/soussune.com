4865ec19cd998e87374b0cef44df0264eb7faf83
Masaya Kazama <miyan@t-p.jp>
2018-03-24 07:32:14 +0900

    Avoid side effect in computed prop

--- pages/actors/index.vue
+++ pages/actors/index.vue
@@ -45,7 +45,7 @@ export default {
   },
   computed: {
     sortedActors() {
-      return this.actors.sort((a, b) => {
+      return this.actors.slice().sort((a, b) => {
         // sort by appearCount or lastAppearDate
         const countDiff = b.episodes.length - a.episodes.length
         return countDiff !== 0 ? countDiff : b.episodes[0].published - a.episodes[0].published

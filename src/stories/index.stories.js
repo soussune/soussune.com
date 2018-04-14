282bd85722391d791772695bcb8d9ea8de416593
trkw <naohiro.terakawa@me.com>
2018-04-14 09:32:44 +0900

    update: Use notes, knobs, vue-info

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -1,25 +1,26 @@
 import { storiesOf } from '@storybook/vue'
+import VueInfoAddon from 'storybook-addon-vue-info'
+import { withKnobs, object, number } from '@storybook/addon-knobs'
+import { withNotes } from '@storybook/addon-notes'
 import ActorIcon from '~/components/ActorIcon.vue'
 
-const actor = {
-  imageUrl:
-    '//images.ctfassets.net/gvze4h7pavn2/55XW80AaGc42cm8SsseAmC/d44f2d51e13bb90347008942d59e8f4c/actor-trkw.jpg',
-  title: 'trkw'
-}
-const noValidSrcActor = {
-  imageUrl: '',
-  title: 'title'
-}
-
 storiesOf('ActorIcon', module)
-  .add('default size(200px)', () => ({
-    render: (h) => h(ActorIcon, { props: { actor } })
-  }))
-  .add('80x80', () => ({
-    components: { ActorIcon },
-    render: (h) => h(ActorIcon, { props: { actor, size: 80 } })
-  }))
-  .add('no valid src', () => ({
-    components: { ActorIcon },
-    render: (h) => h(ActorIcon, { props: { actor: noValidSrcActor } })
-  }))
+  .addDecorator(VueInfoAddon)
+  .addDecorator(withKnobs)
+  .add(
+    'default',
+    withNotes(`WIP`)(() => {
+      const defaultActor = {
+        imageUrl:
+          '//images.ctfassets.net/gvze4h7pavn2/55XW80AaGc42cm8SsseAmC/d44f2d51e13bb90347008942d59e8f4c/actor-trkw.jpg',
+        title: 'trkw'
+      }
+      const actor = object('actor', defaultActor)
+      const size = number('size', 200)
+      return {
+        template: `<ActorIcon :actor="${actor}" :size="${size}" />`,
+        components: { ActorIcon },
+        render: (h) => h(ActorIcon, { props: { actor, size: size } })
+      }
+    })
+  )

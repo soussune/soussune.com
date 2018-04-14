96e104e852c011854a1b5001dafc6a256903d305
trkw <naohiro.terakawa@me.com>
2018-04-14 13:11:29 +0900

    add: figma addon

--- src/stories/AppLogo.stories.js
+++ src/stories/AppLogo.stories.js
@@ -2,11 +2,17 @@ import { storiesOf } from '@storybook/vue'
 import VueInfoAddon from 'storybook-addon-vue-info'
 import { withKnobs } from '@storybook/addon-knobs'
 import { withNotes } from '@storybook/addon-notes'
+import figmaDecorator from 'storybook-addon-figma'
 import AppLogo from '~/components/AppLogo.vue'
 
 storiesOf('AppLogo', module)
   .addDecorator(VueInfoAddon)
   .addDecorator(withKnobs)
+  .addDecorator(
+    figmaDecorator({
+      url: 'https://www.figma.com/file/SsCo9NuNNVITY1KMq5JfJfpx/AppLogo'
+    })
+  )
   .add(
     'default',
     withNotes(`WIP`)(() => {

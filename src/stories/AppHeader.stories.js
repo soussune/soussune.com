0e5e291bccabd3fafb10c5b5959cb537e4bf7532
trkw <naohiro.terakawa@me.com>
2018-04-15 21:57:39 +0900

    :pencil: docs: fixed header pattern for storybook

--- src/stories/AppHeader.stories.js
+++ src/stories/AppHeader.stories.js
@@ -1,6 +1,6 @@
 import { storiesOf } from '@storybook/vue'
 import VueInfoAddon from 'storybook-addon-vue-info'
-import { withKnobs } from '@storybook/addon-knobs'
+import { withKnobs, boolean } from '@storybook/addon-knobs'
 import { withNotes } from '@storybook/addon-notes'
 import AppHeader from '~/components/AppHeader.vue'
 
@@ -10,10 +10,11 @@ storiesOf('AppHeader', module)
   .add(
     'default',
     withNotes(`WIP`)(() => {
+      const fixed = boolean('fixed', false)
       return {
-        template: `<AppHeader class="app_header" />`,
+        template: `<AppHeader class="app_header" :fixed=${fixed} />`,
         components: { AppHeader },
-        render: (h) => h(AppHeader)
+        render: (h) => h(AppHeader, { props: { fixed: fixed } })
       }
     })
   )
